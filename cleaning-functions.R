90f9d3e0ad3a92f68c806a327b3022cf7cd72d04
nicholas-tedesco <nicholas.r.tedesco@gmail.com>
2024-06-10 23:59:04 -0400

    add substance functions

--- cleaning-functions.R
+++ cleaning-functions.R
@@ -5,10 +5,7 @@
 #
 # ------------------------------------------------------------------------------
 
-# packages ---------------------------------------------------------------------
-
-
-# functions for loading data ---------------------------------------------------
+# data loading functions -------------------------------------------------------
 
   ## read in excel doc with multiple sheets
 
@@ -100,7 +97,7 @@
     }
   
     
-# functions for easy data cleaning ---------------------------------------------
+# data cleaning functions ------------------------------------------------------
     
   ## join encounter date to dataset
   
@@ -141,10 +138,12 @@
       # purpose: calculate + add diagnosis variables to data 
       # -------------------------------------------------- # 
       
+      # define variable names based off input 
       date_var_name   = paste0(dx_str, '.date')
       status_var_name = paste0(dx_str, '.status')
       time_var_name   = paste0(dx_str, '.time')
       
+      # for each patient, get diagnosis date (and define status as 1)
       dx_date <- dx_df %>% 
         group_by(PatientID) %>% 
         summarize(
@@ -154,15 +153,160 @@
           {{status_var_name}} := 1
         )
       
+      # join to data; NAs represent diagnosis of 0
       output_df <- working_df %>% 
-        left_join(dx_date, by = 'PatientID') %>% 
+        left_join(dx_date, by = 'PatientID')
+      
+      output_df[is.na(output_df[, status_var_name]), status_var_name] = 0
+      
+      ## derive time variable: if diagnosis: diff(dx, index), else: diff(last_clinic, index)
+      output_df <- output_df %>% 
         mutate(
-          {{time_var_name}} := difftime(.[, date_var_name], date.index, units = 'days')
+          {{time_var_name}} := case_when(
+            .[, status_var_name] == 1 ~ difftime(.[, date_var_name], date.index, units = 'days'),
+            .[, status_var_name] == 0 ~ difftime(date.last.clinic, date.index, units = 'days')
+          )
         )
       
       return(output_df)
       
     }
-  
-  
-  
+    
+  ## add substance variables to existing dataset 
+    
+    add_substance_data <- function(source_str, final_status_str, substance_df, working_df) {
+      
+      # purpose: calculate + add substance status variables to data 
+      # --------------------------------------------------------- # 
+      
+      # collapse substance statuses into four categories, with unknown -> NA 
+      # filter substance df to only nonmissing records
+      
+      unknown = c('Never Assessed', 'Unknown If Ever Smoked', 'Unknown', 'Not Asked', '') 
+      never   = c('Never Smoker', 'Passive Smoke Exposure - Never Smoker', 'Never', 'No')
+      former  = c('Former Smoker', 'Former', 'Not Currently')
+      current = c('Some Days', 'Heavy Smoker', 'Light Smoker', 'Current Some Day Smoker', 'Current Every Day Smoker', 'Light Tobacco Smoker', 'Heavy Tobacco Smoker', 'Smoker, Current Status Unknown', 'Every Day', 'Yes')
+      
+      collapsed_substance_df <- substance_df %>% 
+        select(PatientID, EncounterID, {{source_str}}, AdmitDate) %>% 
+        mutate(
+          status.collapsed = case_when(
+            is.na(.[, source_str]) ~ 'Unknown',
+            .[, source_str] %in% unknown ~ 'Unknown',
+            .[, source_str] %in% never ~ 'Never',
+            .[, source_str] %in% former ~ 'Former',
+            .[, source_str] %in% current ~ 'Current',
+            TRUE ~ 'error'
+          )
+        ) %>% 
+        filter(status.collapsed != 'Unknown') 
+      
+      # join index dates and calculate difference in time
+      # filter to records within +/- one year of index date 
+      dates_substance_df <- collapsed_substance_df %>% 
+        left_join(
+          working_df %>% select(PatientID, date.index), 
+          by = 'PatientID', 
+          relationship = 'many-to-many'
+        ) %>% 
+        mutate(datediff = abs(difftime(AdmitDate, date.index, units = 'days'))) %>% 
+        group_by(PatientID, date.index) %>% 
+        filter(datediff < 365.25) %>% 
+        ungroup()
+      
+      # collapse all values for patient/index combo into one location  
+      substance_per_index <- dates_substance_df %>% 
+        # for each patient + index date combo...
+        group_by(PatientID, date.index) %>% 
+        ## collapse all status values into single variable 
+        summarize(
+          status.values = paste0(status.collapsed, collapse = ' + '), 
+        ) %>% 
+        ungroup() 
+      
+      # create final variable using most positive class 
+      substance_final <- substance_per_index %>% 
+        mutate(
+          {{final_status_str}} := case_when(
+            # current > former > never > unknown
+            grepl('Current', status.values) ~ 'Current', 
+            grepl('Former', status.values) ~ 'Former', 
+            grepl('Never', status.values) ~ 'Never', 
+            TRUE ~ 'error'
+          )
+        ) %>% 
+        select(PatientID, date.index, {{final_status_str}}) %>% 
+        distinct()
+      
+      # join to working data; should be many:one for data:substance
+      output_df <- working_df %>%
+        left_join(
+          substance_final,
+          by = c('PatientID', 'date.index'),
+          relationship = 'many-to-one'
+        )
+      
+      return(output_df)
+      
+    }
+    
+  ## add alcohol usage variables 
+    
+    add_alcohol_data <- function(substance_df, working_df) {
+      
+      # purpose: calculate + add alcohol use amount variables to data 
+      # ----------------------------------------------------------- # 
+      
+      # filter to nonmissing OZ data
+      nonmissing_alcohol <- substance_df %>% 
+        filter(!is.na(AlcoholOZPerWeek))
+      
+      # join index dates 
+      dates_alcohol <- nonmissing_alcohol %>% 
+        left_join(
+          working_df %>% select(PatientID, date.index), 
+          by = 'PatientID', 
+          relationship = 'many-to-many'
+        ) %>% 
+        mutate(datediff = abs(difftime(AdmitDate, date.index, units = 'days'))) %>% 
+        group_by(PatientID, date.index) %>% 
+        filter(datediff < 365.25) %>% 
+        ungroup()
+      
+      # standardize alcohol OZ 
+      standardized_alcohol <- dates_alcohol %>% 
+        rowwise() %>% 
+        # fix records that have range instead of single value
+        mutate(
+          num1 = case_when(
+            grepl('\\-', AlcoholOZPerWeek) ~ gsub('\\-.*', '', AlcoholOZPerWeek), 
+            TRUE ~ AlcoholOZPerWeek
+          ) %>% as.numeric(), 
+          num2 = case_when(
+            grepl('\\-', AlcoholOZPerWeek) ~ gsub('.*\\-', '', AlcoholOZPerWeek), 
+            TRUE ~ AlcoholOZPerWeek
+          ) %>% as.numeric(), 
+          FixedAlcoholOZPerWeek = mean(c(num1, num2))
+        ) %>% 
+        ungroup()
+      
+      # average alcohol consumption for each patient/index combo 
+      alcohol_final <- standardized_alcohol %>% 
+        group_by(PatientID, date.index) %>% 
+        summarize(alcohol.OZ.per.week = mean(FixedAlcoholOZPerWeek)) %>% 
+        mutate(etoh.gm = alcohol.OZ.per.week * 23.33/7) %>% 
+        select(PatientID, date.index, alcohol.OZ.per.week, etoh.gm)
+        
+      # join to working data; should be many:one for data:substance
+      output_df <- working_df %>%
+        left_join(
+          alcohol_final,
+          by = c('PatientID', 'date.index'),
+          relationship = 'many-to-one'
+        )
+      
+      return(output_df)
+        
+    } 
+      
+      
